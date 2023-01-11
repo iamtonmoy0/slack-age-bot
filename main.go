711885d9c056f0aeb7ee6ea2db249425dcee8e1d
iamtonmoy0 <tonmoy2code@gmail.com>
2023-01-11 19:33:56 +0600

    soket token added

---
+++ main.go
@@ -0,0 +1,8 @@
+package main
+
+import "os"
+
+func main() {
+	os.Setenv("slack bot token", "xoxb-4641079431152-4620665186933-Kxo2K1y7hcfwxWcHLIm0XXeq")
+	os.Setenv("slack app token", "xapp-1-A04JQ6MQYQ1-4636230567041-00abef55db3ef05f1dacded3c8cc82c9899c3320e6eba652241355bec0434cd3")
+}
